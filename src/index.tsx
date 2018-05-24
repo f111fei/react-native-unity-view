03266a5a320ccb1c7b2f84dcf268eff21a6e723f
xzper <mail@xzper.com>
2018-05-24 11:14:48 +0800

    add unity message callback

--- src/index.tsx
+++ src/index.tsx
@@ -11,11 +11,66 @@ export interface UnityViewMessageEventData {
     message: string;
 }
 
+export interface UnityViewMessage {
+    name: string;
+    data: any;
+    callBack?: (data: any) => void;
+}
+
+let sequence = 0;
+function generateId() {
+    sequence = sequence + 1;
+    return sequence;
+}
+
+const waitCallbackMessageMap: {
+    [id: number]: UnityViewMessage
+} = {};
+
+const messagePrefix = '@UnityMessage@';
+
 export interface UnityViewProps extends ViewProperties {
     /** 
      * Receive message from unity. 
      */
-    onMessage?: (event: NativeSyntheticEvent<UnityViewMessageEventData>) => void;
+    onMessage?: (message: string) => void;
+    onUnityMessage?: (handler: MessageHandler) => void;
+}
+
+export class MessageHandler {
+    public static deserialize(viewHandler: number, message: string) {
+        const m = JSON.parse(message);
+        const handler = new MessageHandler(viewHandler);
+        handler.id = m.id;
+        handler.seq = m.seq;
+        handler.name = m.name;
+        handler.data = m.data;
+        return handler;
+    }
+
+    public id: number;
+    public seq: 'start' | 'end' | '';
+    public name: string;
+    public data: any;
+
+    private viewHandler: number;
+
+    constructor(viewHandler: number) {
+        this.viewHandler = viewHandler;
+    }
+
+    public send(data: any) {
+        UIManager.dispatchViewManagerCommand(
+            this.viewHandler,
+            UIManager.UnityView.Commands.postMessage,
+            ['UnityMessageManager', 'onUnityMessage', messagePrefix + JSON.stringify({
+                id: this.id,
+                seq: 'end',
+                name: this.name,
+                data: data
+            })]
+        );
+    }
 }
 
 export default class UnityView extends React.Component<UnityViewProps> {
@@ -64,8 +119,21 @@ export default class UnityView extends React.Component<UnityViewProps> {
      * Send Message to UnityMessageManager.
      * @param message The message will post.
      */
-    public postMessageToUnityManager(message: string) {
-        this.postMessage('UnityMessageManager', 'onMessage', message);
+    public postMessageToUnityManager(message: string | UnityViewMessage) {
+        if (typeof message === 'string') {
+            this.postMessage('UnityMessageManager', 'onMessage', message);
+        } else {
+            const id = generateId();
+            if (message.callBack) {
+                waitCallbackMessageMap[id] = message;
+            }
+            this.postMessage('UnityMessageManager', 'onUnityMessage', messagePrefix + JSON.stringify({
+                id: id,
+                seq: message.callBack ? 'start' : '',
+                name: message.name,
+                data: message.data
+            }));
+        }
     };
 
     private getViewHandle() {
@@ -73,8 +141,29 @@ export default class UnityView extends React.Component<UnityViewProps> {
     }
 
     private onMessage(event: NativeSyntheticEvent<UnityViewMessageEventData>) {
-        if (this.props.onMessage) {
-            this.props.onMessage(event);
+        let message = event.nativeEvent.message
+        if (message.startsWith(messagePrefix)) {
+            message = message.replace(messagePrefix, '');
+
+            const handler = MessageHandler.deserialize(this.getViewHandle(), message);
+
+            if (handler.seq === 'end') {
+                // handle callback message
+                const m = waitCallbackMessageMap[handler.id];
+                delete waitCallbackMessageMap[handler.id];
+                if (m && m.callBack != null) {
+                    m.callBack(handler.data);
+                }
+                return;
+            }
+
+            if (this.props.onUnityMessage) {
+                this.props.onUnityMessage(handler);
+            }
+        } else {
+            if (this.props.onMessage) {
+                this.props.onMessage(message);
+            }
         }
     }
 

922b5c4c93e6c1a9306f70e33d3cab0e75747e83
Łukasz Spas <lukasz.spas@live.com>
2018-07-15 12:04:59 +0200

    Updated RNUnityViewBridge plugin.

--- windows/RNUnityViewBridge/RNUnityViewBridge/BridgeBootstrapper.h
+++ windows/RNUnityViewBridge/RNUnityViewBridge/BridgeBootstrapper.h
@@ -2,48 +2,49 @@
 
 namespace RNUnityViewBridge
 {
-	public interface class IDotNetBridge
-	{
-	public:
-		void onMessage(Platform::String^ message);
-	};
+    public interface class IDotNetBridge
+    {
+    public:
+        void onMessage(Platform::String^ message);
+    };
 
-	public interface class IIL2CPPBridge
-	{
-	public:
-		void onMessage(Platform::String^ gameObject, Platform::String^ method, Platform::String^ message);
-	};
+    public interface class IIL2CPPBridge
+    {
+    public:
+        void onMessage(Platform::String^ gameObject, Platform::String^ method, Platform::String^ message);
+        void Shutdown();
+    };
 
-	public ref class BridgeBootstrapper sealed
-	{
-	public:
-		static IDotNetBridge^ GetDotNetBridge()
-		{
-			return m_DotNetBridge;
-		}
+    public ref class BridgeBootstrapper sealed
+    {
+    public:
+        static IDotNetBridge^ GetDotNetBridge()
+        {
+            return m_DotNetBridge;
+        }
 
-		static void SetDotNetBridge(IDotNetBridge^ dotNetBridge)
-		{
-			m_DotNetBridge = dotNetBridge;
-		}
+        static void SetDotNetBridge(IDotNetBridge^ dotNetBridge)
+        {
+            m_DotNetBridge = dotNetBridge;
+        }
 
-		static IIL2CPPBridge^ GetIL2CPPBridge()
-		{
-			return m_IL2CPPBridge;
-		}
+        static IIL2CPPBridge^ GetIL2CPPBridge()
+        {
+            return m_IL2CPPBridge;
+        }
 
-		static void SetIL2CPPBridge(IIL2CPPBridge^ il2cppBridge)
-		{
-			m_IL2CPPBridge = il2cppBridge;
-		}
+        static void SetIL2CPPBridge(IIL2CPPBridge^ il2cppBridge)
+        {
+            m_IL2CPPBridge = il2cppBridge;
+        }
 
-	private:
-		static IDotNetBridge^ m_DotNetBridge;
-		static IIL2CPPBridge^ m_IL2CPPBridge;
+    private:
+        static IDotNetBridge^ m_DotNetBridge;
+        static IIL2CPPBridge^ m_IL2CPPBridge;
 
-		BridgeBootstrapper();
-	};
+        BridgeBootstrapper();
+    };
 
-	IDotNetBridge^ BridgeBootstrapper::m_DotNetBridge;
-	IIL2CPPBridge^ BridgeBootstrapper::m_IL2CPPBridge;
+    IDotNetBridge^ BridgeBootstrapper::m_DotNetBridge;
+    IIL2CPPBridge^ BridgeBootstrapper::m_IL2CPPBridge;
 }

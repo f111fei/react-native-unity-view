0b148fb74f9ef9248996b030f0790f07837d839a
MaTHtoE <canyara@gmail.com>
2018-10-09 21:54:22 +0800

    去除autopoolrelease

--- ios/UnityUtils.mm
+++ ios/UnityUtils.mm
@@ -27,35 +27,32 @@ extern "C" void InitUnity()
     }
     unity_inited = true;
     
-    @autoreleasepool
-    {
-        NSProcessInfo *processInfo = [NSProcessInfo processInfo];
-        int count= (int)[processInfo.arguments count];
-        char** argv = new char*[count];
-        for(int i=0; i<count; i++) {
-            const char* arg = [processInfo.arguments[i] UTF8String];
-            argv[i] = new char[strlen(arg)];
-            strcpy(argv[i], arg);
-        }
-        
-        UnityInitTrampoline();
-        UnityInitRuntime(count, argv);
-        
-        for(int i=0; i<count; i++){
-            delete[] argv[i];
-        }
-        delete[] argv;
-        
-        RegisterMonoModules();
-        NSLog(@"-> registered mono modules %p\n", &constsection);
-        RegisterFeatures();
-        
-        // iOS terminates open sockets when an application enters background mode.
-        // The next write to any of such socket causes SIGPIPE signal being raised,
-        // even if the request has been done from scripting side. This disables the
-        // signal and allows Mono to throw a proper C# exception.
-        std::signal(SIGPIPE, SIG_IGN);
+    NSProcessInfo *processInfo = [NSProcessInfo processInfo];
+    int count= (int)[processInfo.arguments count];
+    char** argv = new char*[count];
+    for(int i=0; i<count; i++) {
+        const char* arg = [processInfo.arguments[i] UTF8String];
+        argv[i] = new char[strlen(arg)];
+        strcpy(argv[i], arg);
     }
+    
+    UnityInitTrampoline();
+    UnityInitRuntime(count, argv);
+    
+    for(int i=0; i<count; i++){
+        delete[] argv[i];
+    }
+    delete[] argv;
+    
+    RegisterMonoModules();
+    NSLog(@"-> registered mono modules %p\n", &constsection);
+    RegisterFeatures();
+    
+    // iOS terminates open sockets when an application enters background mode.
+    // The next write to any of such socket causes SIGPIPE signal being raised,
+    // even if the request has been done from scripting side. This disables the
+    // signal and allows Mono to throw a proper C# exception.
+    std::signal(SIGPIPE, SIG_IGN);
 }
 
 extern "C" void UnityPostMessage(NSString* gameObject, NSString* methodName, NSString* message)
